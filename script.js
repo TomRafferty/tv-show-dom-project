0eb7095855f2a1084b09496a3222dd0c2f880ab9
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-10-24 22:27:35 +0100

    Pseudo added for cards + got rid of some unneeded code.
    this will currently display nothing on the page and simply serves as a base for the build of the site.

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 //You can edit ALL of the code here
 function setup() {
+  //all episodes returns an array of
+  //objects found in episodes.js
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //don't need this right now (not sure if I will)
+  // const rootElem = document.getElementById("root");
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  /*
+  Pseudo:
+  creating the cards for each episode.
+
+  - create a card container
+
+  - for each element in the array:
+
+    . create a div - the card/container
+
+    . create heading element - season+episode+title
+
+    . create img element - img from episode
+
+    . create p element - blurb for episode
+
+  */
 }
 
 window.onload = setup;
